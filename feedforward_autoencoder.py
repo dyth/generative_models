c3826cbd3888173c84fbf2dcbbe48703a1e994a0
dyth <dythui1@gmail.com>
2019-04-01 23:44:08 -0400

    feedforward working

---
+++ feedforward_autoencoder.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+"""
+train an encoder and a decoder
+"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import os
+from tqdm.autonotebook import tqdm
+from torchvision.utils import save_image
+
+from dataloaders import *
+
+torch.manual_seed(9001)
+
+
+class Encoder(nn.Module):
+
+    def __init__(self):
+        'define four layers'
+        super(Encoder, self).__init__()
+        self.fc1 = nn.Linear(784, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 128)
+        self.fc4 = nn.Linear(128, 64)
+
+    def forward(self, x):
+        'five layer neural network'
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
+        return x
+
+
+class Decoder(nn.Module):
+
+    def __init__(self):
+        'define four layers'
+        super(Decoder, self).__init__()
+        self.fc1 = nn.Linear(64, 128)
+        self.fc2 = nn.Linear(128, 256)
+        self.fc3 = nn.Linear(256, 512)
+        self.fc4 = nn.Linear(512, 784)
+
+    def forward(self, x):
+        'five layer neural network'
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.sigmoid(self.fc4(x))
+        return x
+
+
+class Autoencoder(nn.Module):
+
+    def __init__(self):
+        'define encoder and decoder'
+        super(Autoencoder, self).__init__()
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+
+    def forward(self, x):
+        'pass through encoder and decoder'
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+
+
+def train(model, device, train_loader, optimizer, epoch):
+    progress = tqdm(enumerate(train_loader), desc="", total=len(train_loader))
+    model.train()
+    total_loss = 0
+    for i, (data, _) in progress:
+        data = data.view(data.size(0), -1)
+        data = data.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.binary_cross_entropy(output, data)
+        loss.backward()
+        optimizer.step()
+        log_interval = 10
+        total_loss += loss
+        progress.set_description("Loss: {:.4f}".format(total_loss/(i+1)))
+
+
+def test(model, device, test_loader, folder, epoch):
+    model.eval()
+    test_loss = 0
+    with torch.no_grad():
+        for i, (data, _) in enumerate(test_loader):
+            data = data.view(data.size(0), -1)
+            data = data.to(device)
+            output = model(data)
+            test_loss += F.binary_cross_entropy(output, data, reduction='sum').item()
+            test_loss /= len(test_loader.dataset)
+            if i == 0:
+                output = output.view(100, 1, 28, 28)
+                data = data.view(100, 1, 28, 28)
+                save_image(output.cpu(), f'{folder}/{epoch}.png', nrow=10)
+                save_image(data.cpu(), f'{folder}/baseline{epoch}.png', nrow=10)
+        print(f'\nTest set: Average loss: {test_loss:.4f}\n')
+
+
+def main():
+    batch_size = 64
+    test_batch_size = 100
+    epochs = 10
+    save_model = True
+    folder = 'autoencoder'
+
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+    model = Autoencoder().to(device)
+    optimizer = optim.Adam(model.parameters())
+
+    path = 'data'
+    train_loader, test_loader = get_mnist(path, use_cuda, batch_size, test_batch_size)
+
+    for epoch in range(1, epochs + 1):
+        train(model, device, train_loader, optimizer, epoch)
+        test(model, device, test_loader, folder, epoch)
+        if save_model:
+            torch.save(model.state_dict(), f"{folder}/{epoch}.pt")
+
+
+
+if __name__ == '__main__':
+    main()
